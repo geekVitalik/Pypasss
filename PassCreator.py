bd8fc172bfc9a33fc5036f1381bee33b2193fa0b
Vitaliy Chernyshev <vit.chernyshev@gmail.com>
2018-03-22 14:02:28 +0300

    changed variable names

--- PassCreator.py
+++ PassCreator.py
@@ -2,7 +2,7 @@ import random
 import string
 
 letters=string.ascii_letters
-specialCharacters = ['$', '#', '@', '!', '*','_']
+special_characters = ['$', '#', '@', '!', '*','_']
 word=[]
 for i in range(int(input("Enter how many letters you need\n"))):
     word+=[random.choice(letters)]
@@ -10,11 +10,10 @@ for i in range(int(input("Enter how many number's you need\n"))):
     word+=[random.choice("0123456789")]
 for i in range(int(input("Enter how many specials?\n"))):
     word+=[random.choice(specialCharacters)]
-print(word)
 random.shuffle(word)
-print("".join(word))
-saves=input("Enter servise name or just skip this by enter\n")
-if (saves):
+print("Your password is: "+"".join(word))
+service_name=input("Enter servise name or just skip this by enter\n")
+if (service_name):
     with open("This_is_not_secure.txt","a") as f:
         f.write(f"Service name:{saves} "+"security: "+"".join(word)+"\n")
         
